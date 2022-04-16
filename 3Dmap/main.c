7b0df9f3a37a4fe8ebba804495343b4825b342f9
stripy <stripy1026.com>
2022-04-16 18:35:22 +0900

    feat: :sparkles: floor & ceiling raycasting done

--- 3Dmap/main.c
+++ 3Dmap/main.c
@@ -13,6 +13,19 @@
 #define TEXW 64
 #define TEXH 64
 
+/* name of xpm textures */
+#define EAGLE 0
+#define REDBRICK 1
+#define PURPLESTONE 2
+#define GRAYSTONE 3
+#define BLUESTONE 4
+#define MOSSY 5
+#define WOOD 6
+#define COLORSTONE 7
+#define BARRAL 8
+#define PILLAR 9
+#define GREENLIGHT 10
+
 typedef struct s_img
 {
 	void *img;
@@ -34,6 +47,13 @@ typedef struct s_info
 
 	int buf[HEIGHT][WIDTH];
 	int **texture;
+
+	double posX;
+	double posY;
+	double dirX;
+	double dirY;
+	double planeX;
+	double planeY;
 } t_info;
 
 void load_image(t_info *info, int *texture, char *path, t_img *img)
@@ -74,8 +94,58 @@ void draw(t_info *info)
 	mlx_put_image_to_window(info->mlx, info->win, info->img.img, 0, 0);
 }
 
+/* ray casting using lodev algorithm */
+void rayCasting(t_info *info)
+{
+	/* Floor casting */
+	for (int y = HEIGHT / 2 + 1; y < HEIGHT; ++y)
+	{
+		// ray vector for leftmost & rightmost ray
+		float rayDirX0 = info->dirX - info->planeX;
+		float rayDirY0 = info->dirX - info->planeY;
+		float rayDirX1 = info->dirX + info->planeX;
+		float rayDirY1 = info->dirY + info->planeY;
+		// Current y position starts from horizontal half of screen
+		int p = y - HEIGHT / 2;
+		// Vertical position of the camera
+		float posZ = 0.5 * HEIGHT;
+		// Horizontal distance from the camera to the floor for the current row.
+		// 0.5 is the z position exactly in the middle between floor and ceiling.
+		float rowDistance = posZ / p;
+		// calculate the real world step vector we have to add for each x (parallel to camera plane)
+		// adding step by step avoids multiplications with a weight in the inner loop
+		float deltaX = rowDistance * (rayDirX1 - rayDirX0) / WIDTH;
+		float deltaY = rowDistance * (rayDirY1 - rayDirY0) / WIDTH;
+		// real world coordinates of the leftmost column. This will be updated as we step to the right.
+		float floorX = info->posX + rowDistance * rayDirX0;
+		float floorY = info->posY + rowDistance * rayDirY0;
+		for (int x = 0; x < WIDTH; ++x)
+		{
+			int cellX = (int)floorX;
+			int cellY = (int)floorY;
+			// get the texture coordinate from the fractional part
+			int tx = (int)(TEXW * (floorX - cellX)) & (TEXW - 1);
+			int ty = (int)(TEXW * (floorY - cellY)) & (TEXH - 1);
+			// make floor vector point next step.
+			floorX += deltaX;
+			floorY += deltaY;
+			// draw floor
+			int color;
+			color = info->texture[GRAYSTONE][TEXW * ty + tx];
+			color = (color >> 1) & 8355711; // make a bit darker
+			// fill screen buffer with pixel
+			info->buf[y][x] = color;
+			//ceiling (symmetrical, at height - y - 1 instead of y)
+			color = info->texture[WOOD][TEXW * ty + tx];
+			color = (color >> 1) & 8355711; // make a bit darker
+			info->buf[HEIGHT - y - 1][x] = color;
+		}
+	}
+}
+
 int main_loop(t_info *info)
 {
+	rayCasting(info);
 	draw(info);
 	return (0);
 }
@@ -106,6 +176,15 @@ int main()
 	info.img.img = mlx_new_image(info.mlx, WIDTH, HEIGHT);
 	info.img.data = (int *)mlx_get_data_addr(info.img.img, &info.img.bpp, &info.img.size_line, &info.img.endian);
 
+/* set vectors for ray casting */
+	info.posX = 22.0;
+	info.posY = 11.5;
+	info.dirX = -1.0;
+	info.dirY = 0.0;
+	info.planeX = 0.0;
+	info.planeY = 0.66; /* FOV = 67 degree */
+
+/* catching event */
 	mlx_loop_hook(info.mlx, &main_loop, &info);
 	
 	mlx_loop(info.mlx);
